4324ae5fdaab6c22624894b78a6a2e50e154ccce
Vitor Paixão <vitornvpaixao@gmail.com>
2026-09-25 14:26:44 +0100

    chore: config Vite proxy for API

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				changeOrigin: true,
+			},
+		},
+	},
 })
